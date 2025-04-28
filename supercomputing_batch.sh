1ff399202960216527d6d580a8716c0da0b1d64c
Julia C <julia.claxton@colorado.edu>
2025-04-28 15:00:31 -0600

    ready for supercomputing

--- supercomputing_batch.sh
+++ supercomputing_batch.sh
@@ -9,28 +9,6 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jucl6426@colorado.edu
 
-###############################################
-#
-# START FROM HERE IF BUILDING G4EPP FROM SOURCE
-#
-###############################################
-cd /projects/$USER
-cd g4epp-source
-git pull
-
-# Create/reset build directory
-cd /scratch/alpine/$USER
-rm -r /scratch/alpine/$USER/g4epp-build # May fail if already doesn't exist yet
-mkdir /scratch/alpine/$USER/g4epp-build
-
-# Build executable
-cd g4epp-build
-cmake -DCMAKE_INSTALL_PREFIX="/projects/$USER/geant4/install" -DGeant4_DIR="/projects/$USER/geant4/install/lib64" "/projects/$USER/g4epp-source"
-make
-
-# Make batch script executable
-chmod +x ./supercomputing_batch.sh
-
 # Remove previous results
 rm /scratch/alpine/jucl6426/g4epp-build/results/*
 
