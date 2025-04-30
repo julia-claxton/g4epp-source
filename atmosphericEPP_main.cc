3c1cbbb4cb9ed34c375a7a9b5e3a4285214d55bd
Julia C <julia.claxton@colorado.edu>
2025-04-30 13:45:06 -0600

    apr 30

--- atmosphericEPP_main.cc
+++ atmosphericEPP_main.cc
@@ -176,7 +176,7 @@ int main(int argc,char** argv)
   }
   else
   {
-    std::cout << "Incorrect number of command line arguments provided!" << std::endl;
+    std::cout << "Incorrect number of command line arguments provided (" << argc << " given)" std::endl;
     throw;
   }
 
