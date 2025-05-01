859a325aa0e3793edebc389894542c0f6ec6f8d5
Julia C <julia.claxton@colorado.edu>
2025-05-01 11:44:22 -0600

    may 1

--- supercomputing_batch.sh
+++ supercomputing_batch.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name G4EPP
 #SBATCH --nodes 1
-#SBATCH --cpus-per-task 256
+#SBATCH --cpus-per-task 128
 #SBATCH --time 00:05:00
 #SBATCH --output /projects/jucl6426/G4EPP_results/G4EPP_LOG_%j.txt
 #SBATCH --qos=preemptable
