9b120476eb0e753a14335055fc6d25622c0e1d59
Julia C <julia.claxton@colorado.edu>
2025-05-19 13:52:04 -0600

    Mon May 19 13:52:04 MDT 2025

--- create_batch_script.jl
+++ create_batch_script.jl
@@ -1,6 +1,6 @@
 using Statistics, LinearAlgebra
 
-number_of_particles = 100#100000  # Number of particles to input
+number_of_particles = 100000  # Number of particles to input
 
 input_particle = "e-"       # e- = electrons, proton = protons, gamma = photons
 
--- supercomputing_batch.sh
+++ supercomputing_batch.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 40
 #SBATCH --time 50:00:00
-#SBATCH --output /projects/jucl6426/G4EPP_results/results_%j/log.out
+#SBATCH --output /projects/jucl6426/G4EPP/results/results_%j/log.out
 #SBATCH --qos=blanca-lair
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jucl6426@colorado.edu
@@ -15,7 +15,7 @@ echo "
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 40
 #SBATCH --time 50:00:00
-#SBATCH --output /projects/jucl6426/G4EPP_results/results_%j/log.out
+#SBATCH --output /projects/jucl6426/G4EPP/results/results_%j/log.out
 #SBATCH --qos=blanca-lair
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jucl6426@colorado.edu
@@ -25,12 +25,12 @@ echo "
 # https://slurm.schedmd.com/sbatch.html
 
 # Remove previous results
-rm -f /scratch/alpine/jucl6426/g4epp-build/results/*
-cd /scratch/alpine/jucl6426/g4epp-build
+rm -f /projects/jucl6426/G4EPP/build/results/*
+cd /projects/jucl6426/G4EPP/build/
 
 # Execute runs
-for e in 10.0 56.0 316.0 1778.0 10000.0; do
-  for pa in 0.0 18.0 36.0 54.0 72.0 90.0; do
+for e in 10.0 10000.0; do
+  for pa in 0.0 90.0; do
     # Run simulation
     ./G4EPP 100 e- $e $pa
     # Move results
