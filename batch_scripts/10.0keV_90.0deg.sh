aa06d951a16e57ab85ebe97136f67f1dfad0de30
Julia C <julia.claxton@colorado.edu>
2025-05-21 15:23:04 -0600

    Wed May 21 15:23:04 MDT 2025

--- batch_scripts/10.0keV_90.0deg.sh
+++ batch_scripts/10.0keV_90.0deg.sh
@@ -14,6 +14,6 @@ cd /projects/jucl6426/G4EPP/build/
 ./G4EPP 100 e- 10.0 90.0
 
 # Copy results to safe folder
-cp -r /projects/jucl6426/G4EPP/build/results/input_450.0km_record_450.0km/backscatter_electron_input_10.0keV_90.0deg_100particles /projects/jucl6426/G4EPP/results/batch
-cp -r /projects/jucl6426/G4EPP/build/results/input_450.0km_record_450.0km/energy_deposition_electron_input_10.0keV_90.0deg_100particles /projects/jucl6426/G4EPP/results/batch
+cp -r /projects/jucl6426/G4EPP/build/results/input_450.0km_record_450.0km/backscatter_electron_input_10.0keV_90.0deg_100particles.csv /projects/jucl6426/G4EPP/results/batch
+cp -r /projects/jucl6426/G4EPP/build/results/input_450.0km_record_450.0km/energy_deposition_electron_input_10.0keV_90.0deg_100particles.csv /projects/jucl6426/G4EPP/results/batch
 
--- batch_scripts/create_batch_scripts.jl
+++ batch_scripts/create_batch_scripts.jl
@@ -48,8 +48,8 @@ for E in energies_to_simulate
     ./G4EPP $(number_of_particles) e- $(E) $(α)
 
     # Copy results to safe folder
-    cp -r /projects/jucl6426/G4EPP/build/results/input_450.0km_record_450.0km/backscatter_electron_input_$(E)keV_$(α)deg_$(number_of_particles)particles /projects/jucl6426/G4EPP/results/batch
-    cp -r /projects/jucl6426/G4EPP/build/results/input_450.0km_record_450.0km/energy_deposition_electron_input_$(E)keV_$(α)deg_$(number_of_particles)particles /projects/jucl6426/G4EPP/results/batch
+    cp -r /projects/jucl6426/G4EPP/build/results/input_450.0km_record_450.0km/backscatter_electron_input_$(E)keV_$(α)deg_$(number_of_particles)particles.csv /projects/jucl6426/G4EPP/results/batch
+    cp -r /projects/jucl6426/G4EPP/build/results/input_450.0km_record_450.0km/energy_deposition_electron_input_$(E)keV_$(α)deg_$(number_of_particles)particles.csv /projects/jucl6426/G4EPP/results/batch
     """
     )
     close(file)
