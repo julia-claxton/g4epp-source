821181c102455c3f10f1e9ef01a7195521aedff2
Julia C <julia.claxton@colorado.edu>
2025-06-05 18:08:03 -0600

    Thu Jun  5 18:08:03 MDT 2025

--- batch_scripts/create_batch_scripts.jl
+++ batch_scripts/create_batch_scripts.jl
@@ -18,7 +18,7 @@ energies_to_simulate = 10.0 .^ LinRange(log10(energy_kev_min), log10(energy_kev_
 pitch_angles_to_simulate = LinRange(pitch_angle_deg_min, pitch_angle_deg_max, pitch_angle_nbeams)
 
 # TODO delete this. this is just for ELFIN purposes
-pitch_angles_to_simulate = [0.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 61.0, 62.0, 63.0, 64.0, 65.0, 66.0, 67.0, 68.0, 69.0, 70.0, 80.0, 90.0]
+pitch_angles_to_simulate = float.([0:5:60..., 61:1:69..., 70:5:90...])
 energies_to_simulate = [63.245540618896484, 97.97958374023438, 138.5640869140625, 183.30308532714844, 238.11758422851562, 305.20489501953125, 385.16229248046875, 520.48046875, 752.9939575195312, 1081.665283203125, 1529.7060546875, 2121.3203125, 2893.960205078125, 3728.6064453125, 4906.12060546875, 6500.0]
 
 # Round energies and pitch angles to nearest integer for reduced filename verbosity. If you have sub-integer resolution,
@@ -27,10 +27,20 @@ energies_to_simulate = [63.245540618896484, 97.97958374023438, 138.5640869140625
 energies_to_simulate = round.(energies_to_simulate)
 pitch_angles_to_simulate = round.(pitch_angles_to_simulate)
 
+# Don't simulate if we already have data for a given beam
+include("/Users/luna/Research/G4EPP_2.0/Frontend_Functions.jl")
+energies_to_remove, pitch_angles_to_remove = _get_beam_locations(data_type = "raw")
+skipped = 0
+
 # Create shell scripts
 rm.(glob("*deg.sh", @__DIR__))
 for E in energies_to_simulate
   for α in pitch_angles_to_simulate
+    if (E, α) ∈ collect(zip(energies_to_remove, pitch_angles_to_remove))
+      global skipped += 1
+      continue
+    end
+
     job_name = "$(E)keV_$(α)deg"
     qos = "preemptable"
     time_limit = "1-00:00:00"
@@ -71,3 +81,5 @@ for E in energies_to_simulate
     close(file)
   end
 end
+
+println("Skipped $(skipped)/$(length(energies_to_simulate)*length(pitch_angles_to_simulate)) files.")
