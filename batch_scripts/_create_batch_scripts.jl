4228a89a8d26aa9c7d103036f31b3db1ba5cdbf2
Julia C <julia.claxton@colorado.edu>
2025-09-03 15:36:23 -0600

    Wed Sep  3 21:36:23 UTC 2025

--- batch_scripts/_create_batch_scripts.jl
+++ batch_scripts/_create_batch_scripts.jl
@@ -4,75 +4,79 @@ using Printf
 
 number_of_particles = 100_000  # Number of particles to input
 
-input_particles = ["proton"]          # "e-" = electrons, "proton" = protons, "gamma" = photons
+particle = "e-"          # "e-" = electrons, "proton" = protons, "gamma" = photons
 
 # Create energy and pitch angle lists
-energy_kev_min = 100            # Minimum beam energy, keV
-energy_kev_max = 1_000_000           # Maximum beam energy, keV
-energy_nbeams = 50             # Number of log-spaced beams to place between minimum and maximum energy
+energy_kev_min = 1            # Minimum beam energy, keV
+energy_kev_max = 10_000       # Maximum beam energy, keV
+energy_nbeams = 30             # Number of log-spaced beams to place between minimum and maximum energy
 energies_to_simulate = 10.0 .^ LinRange(log10(energy_kev_min), log10(energy_kev_max), energy_nbeams)
 
 pitch_angle_deg_min = 0        # Minimum beam pitch angle, deg
 pitch_angle_deg_max = 90       # Maximum beam pitch angle, deg
-pitch_angle_nbeams = 10        # Number of linear-spaced beams to place between minimum and maximum pitch angle
+pitch_angle_nbeams = 19        # Number of linear-spaced beams to place between minimum and maximum pitch angle
 pitch_angles_to_simulate = LinRange(pitch_angle_deg_min, pitch_angle_deg_max, pitch_angle_nbeams)
 
 
 # Round energies and pitch angles to nearest integer for reduced filename verbosity. If you have sub-integer resolution,
 # this will eat that, so if you're doing < 1º pitch angle resolution or < 1 keV energy resolution, remove this block. Note
 # that removal of this block may result in very long output filenames.
-energies_to_simulate = round.(energies_to_simulate)
-pitch_angles_to_simulate = round.(pitch_angles_to_simulate)
+energies_to_simulate = round.(energies_to_simulate, digits = 1)
+pitch_angles_to_simulate = round.(pitch_angles_to_simulate, digits = 1)
 
-# Don't simulate if we already have data for a given beam
-# TODO
-skipped = 0
 
 # Create shell scripts
 rm.(glob("*deg.sh", @__DIR__))
 written = 0
+skipped = 0
 
-for particle in input_particles
-  for E in energies_to_simulate
-    for α in pitch_angles_to_simulate
-      input_particle_longname = particle == "e-" ? "electron" : particle
-      energy_string = @sprintf "%.1f" E
-      job_name = "$(particle)_$(energy_string)keV_$(α)deg"
-      qos = "preemptable"
-      time_limit = "1-00:00:00"
+for E in energies_to_simulate
+  for α in pitch_angles_to_simulate
+    input_particle_longname = particle == "e-" ? "electron" : particle
+    energy_string = @sprintf "%.1f" E
+    job_name = "$(input_particle_longname)_$(energy_string)keV_$(α)deg"
+    qos = "preemptable"
+    time_limit = "1-00:00:00"
 
-      file = open("$(@__DIR__)/$(job_name).sh", "w")
-      println(file,
-      """
-      #!/bin/bash
+    # Don't simulate if we already have data for a given beam
+    # TODO
 
-      #SBATCH --job-name G4EPP_$(job_name)
-      #SBATCH --nodes 1
-      #SBATCH --ntasks-per-node 40
-      #SBATCH --time $(time_limit)
-      #SBATCH --output /projects/jucl6426/G4EPP/results/log_$(job_name).out
-      #SBATCH --qos=$(qos)
-      #SBATCH --exclude=bhpc-c5-u7-22,bhpc-c5-u7-23
-      #SBATCH --requeue
-      #SBATCH --mail-type=ALL
-      #SBATCH --mail-user=jucl6426@colorado.edu
-
-      # Terminate on any non-zero exit status
-      set -e
-
-      # Run simulation
-      cd /projects/jucl6426/G4EPP/build/
-      ./G4EPP $(number_of_particles) $(particle) $(energy_string) $(α)
-
-      # Copy results to safe folder
-      cp /projects/jucl6426/G4EPP/build/results/input_449.5km_record_450.5km/backscatter_$(input_particle_longname)_input_$(energy_string)keV_$(α)deg_$(number_of_particles)particles.csv /projects/jucl6426/G4EPP/results
-      cp /projects/jucl6426/G4EPP/build/results/input_449.5km_record_450.5km/energy_deposition_$(input_particle_longname)_input_$(energy_string)keV_$(α)deg_$(number_of_particles)particles.csv /projects/jucl6426/G4EPP/results
-      """
-      )
-      close(file)
-
-      global written += 1
+    if (65 ≤ α ≤ 72) && (E < 200)
+      qos = "blanca-lair"
+      time_limit = "7-00:00:00"
     end
+
+    file = open("$(@__DIR__)/$(job_name).sh", "w")
+    println(file,
+    """
+    #!/bin/bash
+
+    #SBATCH --job-name G4EPP_$(job_name)
+    #SBATCH --nodes 1
+    #SBATCH --ntasks-per-node 40
+    #SBATCH --time $(time_limit)
+    #SBATCH --output /projects/jucl6426/G4EPP/results/log_$(job_name).out
+    #SBATCH --qos=$(qos)
+    #SBATCH --exclude=bhpc-c5-u7-22,bhpc-c5-u7-23
+    #SBATCH --requeue
+    #SBATCH --mail-type=ALL
+    #SBATCH --mail-user=jucl6426@colorado.edu
+
+    # Terminate on any non-zero exit status
+    set -e
+
+    # Run simulation
+    cd /projects/jucl6426/G4EPP/build/
+    ./G4EPP $(number_of_particles) $(particle) $(energy_string) $(α)
+
+    # Copy results to safe folder
+    cp /projects/jucl6426/G4EPP/build/results/input_449.5km_record_450.5km/backscatter_$(input_particle_longname)_input_$(energy_string)keV_$(α)deg_$(number_of_particles)particles.csv /projects/jucl6426/G4EPP/results
+    cp /projects/jucl6426/G4EPP/build/results/input_449.5km_record_450.5km/energy_deposition_$(input_particle_longname)_input_$(energy_string)keV_$(α)deg_$(number_of_particles)particles.csv /projects/jucl6426/G4EPP/results
+    """
+    )
+    close(file)
+
+    global written += 1
   end
 end
 
---
+++ batch_scripts/electron_85.3keV_85.0deg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name G4EPP_electron_85.3keV_85.0deg
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 40
+#SBATCH --time 1-00:00:00
+#SBATCH --output /projects/jucl6426/G4EPP/results/log_electron_85.3keV_85.0deg.out
+#SBATCH --qos=preemptable
+#SBATCH --exclude=bhpc-c5-u7-22,bhpc-c5-u7-23
+#SBATCH --requeue
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jucl6426@colorado.edu
+
+# Terminate on any non-zero exit status
+set -e
+
+# Run simulation
+cd /projects/jucl6426/G4EPP/build/
+./G4EPP 100000 e- 85.3 85.0
+
+# Copy results to safe folder
+cp /projects/jucl6426/G4EPP/build/results/input_449.5km_record_450.5km/backscatter_electron_input_85.3keV_85.0deg_100000particles.csv /projects/jucl6426/G4EPP/results
+cp /projects/jucl6426/G4EPP/build/results/input_449.5km_record_450.5km/energy_deposition_electron_input_85.3keV_85.0deg_100000particles.csv /projects/jucl6426/G4EPP/results
+
